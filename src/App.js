68c55e1fc9c46a6f3d44071366aaacee8956b440
Kutay Erdoğan <80005211+kutayerdogan@users.noreply.github.com>
2023-05-22 10:48:04 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+
+const App = () => {
+  const [images, setImages] = useState(null)
+  const [value, setValue] = useState(null)
+  const [error, setError] = useState(null)
+  const surpriseOptions = [
+    'a dog in a car',
+    'a cat in a hat',
+    'a bird in a tree',
+    'a fish tank',
+    'a horse in a field',
+  ];
+
+  const surpriseMe = () => {
+    setImages(null)
+    const randomValue = surpriseOptions[Math.floor(Math.random() * surpriseOptions.length)]
+    setValue(randomValue)
+  }
+
+  const getImages = async () => {
+    setImages(null)
+    if (value == null) {
+      setError('Error! Must have a search term')
+      return
+    }
+    try {
+      const response = await fetch('http://localhost:8000/images', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ prompt: value }),
+      });
+      const data = await response.json();
+      console.log(data);
+      setImages(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  console.log(value);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="search-section">
+        <p>Start with a detailed description 
+          <span className='surprise' onClick={surpriseMe}>Surprise me</span>
+          </p>
+          <div className='input-container'>
+            <input
+            value={value} 
+            placeholder='An impressionist oil painting of a sunflower in a purple vase' onChange={(e) => setValue(e.target.value)}/>
+            <button onClick={getImages}>Generate</button>
+          </div>  
+          {error && <p className='error'>{error}</p>}
+      </section>
+      
+      <section className="image-section">
+        {images?.map((image, index) => (
+          <img key={index} src={image.url} alt={`Generated image of ${value}`}/>))
+        }
+      </section>
+
     </div>
   );
 }
